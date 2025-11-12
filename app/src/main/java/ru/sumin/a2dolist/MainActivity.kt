0296802d82e878c4051e05b8ebce3d06752de8ae
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-11-12 21:58:29 +0300

    Initial commit

---
+++ app/src/main/java/ru/sumin/a2dolist/MainActivity.kt
@@ -0,0 +1,25 @@
+package ru.sumin.a2dolist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.sumin.a2dolist.ui.theme._2DoListTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _2DoListTheme {
+            }
+        }
+    }
+}
