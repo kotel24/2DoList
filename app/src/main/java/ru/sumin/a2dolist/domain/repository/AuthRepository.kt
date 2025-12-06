aa56185a55cf4a9e945cc4feb296adbdeb528c96
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-12-06 10:36:43 +0300

    feat(domain): add Auth interfaces

--- app/src/main/java/ru/sumin/a2dolist/domain/repository/AuthRepository.kt
+++ app/src/main/java/ru/sumin/a2dolist/domain/repository/AuthRepository.kt
@@ -2,15 +2,21 @@ package ru.sumin.a2dolist.domain.repository
 
 import kotlinx.coroutines.flow.Flow
 
-interface LoginRepository {
+interface AuthRepository {
 
     suspend fun login(email: String, password: String): Result<Unit>
 
     suspend fun register(email: String, password: String): Result<Unit>
 
-    fun observeAuthToken(): Flow<String?>
+    fun observeAuthToken(): Flow<Boolean>
 
     suspend fun isUserLoggedIn(): Boolean
 
     suspend fun logout()
+
+    suspend fun sendEmail(email: String): Result<Unit>
+
+    suspend fun forgotPassword(email: String): Result<Unit>
+
+    suspend fun validateEmail(code: String, email: String): Result<Unit>
 }
